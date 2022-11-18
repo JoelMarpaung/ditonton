d82888a5cba5464708882dc883946d314399087a
Joel Marpaung <myitlife.jm@gmail.com>
2022-11-18 13:37:29 +0700

    adding usecase test

---
+++ test/domain/usecases/get_season_detail_test.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/usecases/get_season_detail.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../dummy_data/dummy_objects.dart';
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetSeasonDetail usecase;
+  late MockTvRepository mockTvRepository;
+
+  setUp(() {
+    mockTvRepository = MockTvRepository();
+    usecase = GetSeasonDetail(mockTvRepository);
+  });
+
+  final tId = 1;
+  final tSeasonNumber = 1;
+
+  test('should get Season detail from the repository', () async {
+    // arrange
+    when(mockTvRepository.getSeasonDetail(tId, tSeasonNumber))
+        .thenAnswer((_) async => Right(testSeasonDetail));
+    // act
+    final result = await usecase.execute(tId, tSeasonNumber);
+    // assert
+    expect(result, Right(testSeasonDetail));
+  });
+}
